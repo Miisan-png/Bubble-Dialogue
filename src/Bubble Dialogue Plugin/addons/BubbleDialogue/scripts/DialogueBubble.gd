5bfc3a7e50e5b03d219335ee811f882effaee4a3
Miisan <128460054+Rena-afk-c@users.noreply.github.com>
2024-08-13 15:25:59 +0800

    feat: added new commands color change and wait
    Added two more commands after dialogue to change color and wait for text within dialogue, fixed effect on next line. Changed text label to RichtextLabel
    
    - Implement color change using BBCode
    - Update tag processing to handle color tags
    - Maintain shake effect for whole dialogue box
    - Improve code readability and maintainability
    
    Usage example:
    Miisan: This whole box will shake! [shake:1.0]
    Miisan: And now [color:#8affd6] these characters change color [/color]

--- src/Bubble Dialogue Plugin/addons/BubbleDialogue/scripts/DialogueBubble.gd
+++ src/Bubble Dialogue Plugin/addons/BubbleDialogue/scripts/DialogueBubble.gd
@@ -7,12 +7,21 @@ extends Panel
 
 var current_dialogue: DialogueResource = null
 var tween: Tween
+var current_text: String = ""
+var displayed_text: String = ""
+var char_index: int = 0
+var waiting: bool = false
+var current_effect: String = ""
+var effect_strength: float = 0.0
 var shake_tween: Tween
+var default_text_color: Color
 
 func _ready():
 	dialogue_manager.connect("dialogue_started", Callable(self, "_on_dialogue_started"))
 	dialogue_manager.connect("dialogue_ended", Callable(self, "_on_dialogue_ended"))
 	dialogue_manager.connect("dialogue_text_changed", Callable(self, "_on_dialogue_text_changed"))
+	default_text_color = text_label.get_theme_color("font_color")
+	text_label.bbcode_enabled = true
 	hide()
 
 func play(dialogue_input):
@@ -27,43 +36,87 @@ func _on_dialogue_started():
 	show()
 	_animate_show_dialogue_box()
 
-func _on_dialogue_ended():
-	_animate_hide_dialogue_box()
-	if shake_tween:
-		shake_tween.kill()
-
 func _on_dialogue_text_changed(character: String, new_text: String):
 	character_name_label.text = character + ":"
-	_animate_text_appearance(new_text)
-	
-	var shake_start = new_text.find("[shake:")
-	if shake_start != -1:
-		var shake_end = new_text.find("]", shake_start)
-		if shake_end != -1:
-			var shake_value = new_text.substr(shake_start + 7, shake_end - shake_start - 7)
-			var shake_speed = float(shake_value)
-			_apply_shake_effect(shake_speed)
-		
-		new_text = new_text.substr(0, shake_start) + new_text.substr(shake_end + 1)
-	
-	text_label.text = new_text
+	current_text = new_text
+	displayed_text = ""
+	char_index = 0
+	waiting = false
+	current_effect = ""
+	effect_strength = 0.0
+	text_label.text = ""
+	_stop_all_effects()
+	_process_next_character()
 
-func _apply_shake_effect(speed: float):
+func _process_next_character():
+	if char_index >= current_text.length():
+		_start_indicator_animation()
+		return
+
+	var tag_start = current_text.find("[", char_index)
+	if tag_start == char_index:
+		var tag_end = current_text.find("]", tag_start)
+		if tag_end != -1:
+			var tag = current_text.substr(tag_start + 1, tag_end - tag_start - 1)
+			_process_tag(tag)
+			char_index = tag_end + 1
+		else:
+			char_index += 1
+	else:
+		var new_char = current_text[char_index]
+		displayed_text += new_char
+		text_label.text = displayed_text
+		char_index += 1
+
+	if not waiting:
+		_schedule_next_character()
+
+func _schedule_next_character():
+	tween = create_tween()
+	tween.tween_callback(Callable(self, "_process_next_character")).set_delay(0.05)
+
+func _process_tag(tag: String):
+	if tag.begins_with("wait:"):
+		var wait_time = float(tag.split(":")[1])
+		waiting = true
+		tween = create_tween()
+		tween.tween_callback(Callable(self, "_resume_after_wait")).set_delay(wait_time)
+	elif tag.begins_with("shake:"):
+		effect_strength = float(tag.split(":")[1])
+		_apply_shake_effect()
+	elif tag.begins_with("color:"):
+		var color = tag.split(":")[1]
+		displayed_text += "[color=" + color + "]"
+	elif tag == "/color":
+		displayed_text += "[/color]"
+
+func _resume_after_wait():
+	waiting = false
+	_schedule_next_character()
+
+func _apply_shake_effect():
 	if shake_tween:
 		shake_tween.kill()
 	
 	var original_position = position
 	shake_tween = create_tween()
 	shake_tween.set_loops()
-	shake_tween.tween_property(self, "position:y", original_position.y - 5, 0.05 / speed)
-	shake_tween.tween_property(self, "position:y", original_position.y, 0.05 / speed)
+	shake_tween.tween_property(self, "position", original_position + Vector2(5, 0), 0.05 / effect_strength)
+	shake_tween.tween_property(self, "position", original_position + Vector2(-5, 0), 0.1 / effect_strength)
+	shake_tween.tween_property(self, "position", original_position, 0.05 / effect_strength)
 
+func _stop_all_effects():
+	if shake_tween:
+		shake_tween.kill()
+	position = Vector2.ZERO
 
+func _on_dialogue_ended():
+	_stop_all_effects()
+	_animate_hide_dialogue_box()
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_accept") and visible:
 		dialogue_manager.show_next_dialogue()
-
 
 func _animate_show_dialogue_box():
 	self.modulate.a = 0
@@ -80,14 +133,6 @@ func _animate_hide_dialogue_box():
 	tween.tween_property(self, "modulate:a", 0.0, 0.3)
 	tween.tween_property(self, "scale", Vector2(0.5, 0.5), 0.3).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
 	tween.tween_callback(Callable(self, "hide"))
-
-func _animate_text_appearance(new_text: String):
-	text_label.visible_characters = 0
-	text_label.text = new_text
-	
-	tween = create_tween()
-	tween.tween_property(text_label, "visible_characters", new_text.length(), 0.5)
-	tween.tween_callback(Callable(self, "_start_indicator_animation"))
 
 func _start_indicator_animation():
 	tween = create_tween()
